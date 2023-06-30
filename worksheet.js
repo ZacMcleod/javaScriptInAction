465d5cc8fc792feb6fa29c3252e94e9174abcfce
ZacMcleod <zacamcleod33@gmail.com>
2023-06-30 12:11:21 -0700

    test

---
+++ worksheet.js
@@ -0,0 +1,9 @@
+let myName = "Zac";
+
+console.log(`Hello world! My name is ${myName}`);
+
+myName = prompt("What is your name: ");
+
+console.log(`So.. Your name is ${myName}`);
+
+alert(`Hello ${myName}`);
